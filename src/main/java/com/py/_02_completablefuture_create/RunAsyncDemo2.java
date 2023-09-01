a9ea7ce18944db295026dd9224307841ea62f728
pengyang <20844@qq.com>
2023-09-01 21:53:41 +0800

    开启异步任务之runAsync

---
+++ src/main/java/com/py/_02_completablefuture_create/RunAsyncDemo2.java
@@ -0,0 +1,39 @@
+package com.py._02_completablefuture_create;
+
+import com.py.utils.CommonUtils;
+
+import java.util.concurrent.CompletableFuture;
+
+public class RunAsyncDemo2 {
+    public static void main(String[] args) {
+
+
+        //开启一个异步任务读取文件
+        /**
+         * CompletableFuture 中的异步任务底层通过开启线程的方式完成的
+         */
+        // runAsync 创建异步任务
+        CommonUtils.printThreadLog("main start");
+        // 使用Runnable匿名内部类
+        // CompletableFuture.runAsync(new Runnable() {
+        //     @Override
+        //     public void run() {
+        //         CommonUtils.printThreadLog("读取文件开始");
+        //         //使用睡眠来模拟一个长时间的工作任务（例如读取文件，网络请求等）
+        //         CommonUtils.sleepSecond(3);
+        //         CommonUtils.printThreadLog("读取文件结束");
+        //     }
+        // });
+
+        // 使用lambda表达式
+        CompletableFuture.runAsync(()->{
+                CommonUtils.printThreadLog("读取文件开始");
+                //使用睡眠来模拟一个长时间的工作任务（例如读取文件，网络请求等）
+                CommonUtils.sleepSecond(3);
+                CommonUtils.printThreadLog("读取文件结束");
+        });
+        CommonUtils.printThreadLog("here are not blocked,mian continue");
+        CommonUtils.sleepSecond(4);//此处休眠为的是等待completableFuture 背后的线程池执行完成。
+        CommonUtils.printThreadLog("main end");
+    }
+}
---
+++ src/main/java/com/py/_02_completablefuture_create/RunAsyncDemo3.java
@@ -0,0 +1,29 @@
+package com.py._02_completablefuture_create;
+
+import com.py.utils.CommonUtils;
+
+import java.util.concurrent.CompletableFuture;
+
+public class RunAsyncDemo3 {
+    public static void main(String[] args) {
+        //需求：使用多线程异步读取 news.txt打印输出。
+        CommonUtils.printThreadLog("main start");
+
+        CompletableFuture.runAsync(()->{
+            CommonUtils.printThreadLog("读取文件开始");
+                //使用睡眠来模拟一个长时间的工作任务（例如读取文件，网络请求等）
+            String content = CommonUtils.readFile("news.txt");
+            CommonUtils.printThreadLog(content);
+        });
+        CommonUtils.printThreadLog("here are not blocked,mian continue");
+        CommonUtils.sleepSecond(4);
+        CommonUtils.printThreadLog("main end");
+    }
+
+    /**
+     * 疑问：异步任务是并发执行还是并行执行？
+     * 如果是单核CPU，那么异步任务之间就是并发执行；如果是多核CPU(多CPU)异步任务就是并行执行
+     * 重点(敲黑板）
+     * 作为开发者，我们只需要清楚如何开启异步任务，CPU 硬件会把异步任务合理的分配给CPU上的核运行。
+     */
+}
